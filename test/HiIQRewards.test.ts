a2ff03034fc58160669a71723241e67ad5c3e339
Cesar Rodriguez <kesarr@gmail.com>
2021-07-22 01:11:51 +0200

    fixed tests

--- test/HiIQRewards.test.ts
+++ test/HiIQRewards.test.ts
@@ -64,35 +64,41 @@ describe('HiIQRewards', () => {
     const user = users[0];
     const lockTime = Math.round(new Date().getTime() / 1000) + 6000000;
     const amount = 10e8;
-    const lockedAmount = amount/2;
-    const rewardAmount = amount/2;
+    const lockedAmount = amount / 2;
+    const rewardAmount = amount / 2;
 
     await expect(deployer.HiIQRewards.initializeDefault()).to.be.not.reverted;
 
-    await expect(deployer.IQERC20.mint(user.address, amount)).to.be.not.reverted;
+    await expect(deployer.IQERC20.mint(user.address, amount)).to.be.not
+      .reverted;
 
-    await expect(user.IQERC20.approve(HIIQ.address, lockedAmount)).to.be.not.reverted;
+    await expect(user.IQERC20.approve(HIIQ.address, lockedAmount)).to.be.not
+      .reverted;
 
-    await expect(user.HIIQ.create_lock(lockedAmount, lockTime)).to.be.not.reverted;
+    await expect(user.HIIQ.create_lock(lockedAmount, lockTime)).to.be.not
+      .reverted;
 
-    await expect(user.IQERC20.transfer(HiIQRewards.address, rewardAmount)
-    ).to.be.not.reverted;
+    await expect(user.IQERC20.transfer(HiIQRewards.address, rewardAmount)).to.be
+      .not.reverted;
 
-    await expect(deployer.HiIQRewards.setYieldRate(100000, true)).to.be.not.reverted;
+    await expect(deployer.HiIQRewards.setYieldRate(100000, true)).to.be.not
+      .reverted;
 
     await ethers.provider.send('evm_increaseTime', [120000]);
     await ethers.provider.send('evm_mine', []);
 
     const currentBalance = amount - lockedAmount - rewardAmount;
-    expect(await deployer.IQERC20.balanceOf(user.address)).to.equal(currentBalance);
-    expect(
-      await user.HiIQRewards['userIsInitialized(address)'](user.address)
-    ).to.be.false;
+    expect(await deployer.IQERC20.balanceOf(user.address)).to.equal(
+      currentBalance
+    );
+    expect(await user.HiIQRewards['userIsInitialized(address)'](user.address))
+      .to.be.false;
     await user.HiIQRewards['checkpoint()']();
     await user.HiIQRewards['getYield()']();
-    expect(
-      await user.HiIQRewards['userIsInitialized(address)'](user.address)
-    ).to.be.true;
-    expect(await deployer.IQERC20.balanceOf(user.address)).to.not.equal(currentBalance);
+    expect(await user.HiIQRewards['userIsInitialized(address)'](user.address))
+      .to.be.true;
+    expect(await deployer.IQERC20.balanceOf(user.address)).to.not.equal(
+      currentBalance
+    );
   });
 });
