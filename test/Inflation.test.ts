9e748a0b7f7088d2e97d4f9024560f0d4a8eb833
Cesar Rodriguez <kesarr@gmail.com>
2022-11-30 17:45:44 +0100

    adding tests

--- test/Inflation.test.ts
+++ test/Inflation.test.ts
@@ -7,7 +7,8 @@ import {
 } from 'hardhat';
 import {DummyERC20, Inflation, IQERC20} from '../typechain';
 import {setupUser, setupUsers} from './utils';
-import {BigNumber} from 'ethers';
+import {parseEther} from 'ethers/lib/utils';
+import {expect} from 'chai';
 
 const setup = deployments.createFixture(async () => {
   const {deployer} = await getNamedAccounts();
@@ -43,17 +44,70 @@ describe('Inflation', function () {
       params: [],
     });
   });
-  //TODO: add tests
-  it('Inflation can inflate', async function () {
+
+  it('Admin can changeInflation', async function () {
     const {users, deployer} = await setup();
-    console.log(await deployer.Inflation.rewards());
-    await deployer.Inflation.changeInflation([
-      {
-        destination: users[0].address,
-        emissionsPerSecond: BigNumber.from('1000000000000000000'),
-      },
-    ]);
-    console.log(await deployer.Inflation.rewards());
-    // TODO: test change inflation, permissions, rescue, inflate
+    const rewards = {
+      destination: users[0].address,
+      emissionsPerSecond: parseEther('1'),
+    };
+    await deployer.Inflation.changeInflation([rewards]);
+    const storedRewards = await deployer.Inflation.rewards();
+    expect(storedRewards[0].emissionsPerSecond).to.be.equal(
+      rewards.emissionsPerSecond
+    );
+  });
+
+  it('Admin actions only admin', async function () {
+    const {users} = await setup();
+    const nonOwner = users[0];
+    await expect(nonOwner.Inflation.changeInflation([])).to.be.revertedWith(
+      'NotOwner()'
+    );
+    await expect(
+      nonOwner.Inflation.rescue(users[1].address, 0)
+    ).to.be.revertedWith('NotOwner()');
+    await expect(
+      nonOwner.Inflation.transferOwnership(users[1].address)
+    ).to.be.revertedWith('NotOwner()');
+  });
+
+  it('Admin can rescue', async function () {
+    const {deployer} = await setup();
+
+    await deployer.IQERC20.mint(deployer.address, 1000);
+    await deployer.IQERC20.transfer(deployer.Inflation.address, 1000);
+    expect(await deployer.IQERC20.balanceOf(deployer.address)).to.equal(0);
+    await deployer.Inflation.rescue(deployer.IQERC20.address, 1000);
+    expect(await deployer.IQERC20.balanceOf(deployer.address)).to.equal(1000);
+  });
+
+  it('Inflate', async function () {
+    const {users, deployer} = await setup();
+    const destination = users[0].address;
+    const lastClaimed = await deployer.Inflation.lastClaimed();
+    await deployer.IQERC20.mint(deployer.Inflation.address, parseEther('603'));
+    const rewards = {
+      destination: destination,
+      emissionsPerSecond: parseEther('1'),
+    };
+    await deployer.Inflation.changeInflation([rewards]);
+    await ethers.provider.send('evm_increaseTime', [600]);
+
+    await deployer.Inflation.inflate();
+    expect(await deployer.Inflation.lastClaimed()).to.be.equal(
+      lastClaimed.add(603)
+    );
+    expect(await deployer.IQERC20.balanceOf(destination)).to.equal(
+      parseEther('603')
+    );
+
+    await expect(deployer.Inflation.inflate()).to.be.revertedWith(
+      'MaxOneCallEachTenMinutes()'
+    );
+
+    await ethers.provider.send('evm_increaseTime', [600]);
+    await deployer.IQERC20.mint(deployer.Inflation.address, parseEther('603'));
+    await deployer.Inflation.inflate();
   });
 });
