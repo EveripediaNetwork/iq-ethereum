81037c59c067f6c5cb38b3ab2a171695e44d653b
César Rodríguez <kesarr@gmail.com>
2024-05-07 13:24:01 +0200

    Update WIQ.sol

--- src/ERC20/WIQ.sol
+++ src/ERC20/WIQ.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0
-pragma solidity 0.7.1;
+pragma solidity ^0.4.18;
 
 contract WIQ {
     string public name     = "Wrapped IQ";
@@ -20,23 +20,23 @@ contract WIQ {
     
     function deposit() public payable {
         balanceOf[msg.sender] += msg.value;
-        Deposit(msg.sender, msg.value);
+        emit Deposit(msg.sender, msg.value);
     }
     
     function withdraw(uint wad) public {
         require(balanceOf[msg.sender] >= wad);
         balanceOf[msg.sender] -= wad;
         msg.sender.transfer(wad);
-        Withdrawal(msg.sender, wad);
+        emit Withdrawal(msg.sender, wad);
     }
 
     function totalSupply() public view returns (uint) {
-        return this.balance;
+        return address(this).balance;
     }
 
     function approve(address guy, uint wad) public returns (bool) {
         allowance[msg.sender][guy] = wad;
-        Approval(msg.sender, guy, wad);
+        emit Approval(msg.sender, guy, wad);
         return true;
     }
 
@@ -58,7 +58,7 @@ contract WIQ {
         balanceOf[src] -= wad;
         balanceOf[dst] += wad;
 
-        Transfer(src, dst, wad);
+        emit Transfer(src, dst, wad);
 
         return true;
     }
